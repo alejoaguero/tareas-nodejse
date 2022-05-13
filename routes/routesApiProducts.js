c76bc7aa3347c114153817cbe93f14a5e86ecfbd
Alejo <alejoleonel45@gmail.com>
2022-05-13 01:07:52 -0300

    Desafio de api restful

--- routes/routesApiProducts.js
+++ routes/routesApiProducts.js
@@ -1,29 +1,84 @@
 const {Router} = require('express');
 const router = Router();
-const {Productos} = require('../controllers/classProducst');
-const {apiProductos} = require('../db/Productos');
 
-const Product = new Productos(apiProductos);
+// Array de Productos
+let productos = [
+    {
+        id: 1,
+        name: 'Producto 1',
+        price: 100,
+        description: 'Descripcion del producto 1'
+    },
+    {
+        id: 2,
+        name: 'Producto 2',
+        price: 200,
+        description: 'Descripcion del producto 3'
+    }
+]
 
-router.get('/', Product.getAllProdu());
+
+
+// Clase de Producto
+
+class Product {
+    constructor(id,name,price,description,image){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
+
+
+
+// Rutas
+router.get('/',(req,res)=>{
+    res.json(productos)
+});
+
+router.post('/', (req, res) => {
+    let nuevoProdu = {};
+    nuevoProdu = new Product();
+
+    nuevoProdu.id = productos[productos.length-1].id + 1;
+    nuevoProdu.name = req.body.name;
+    nuevoProdu.description = req.body.description;
+    nuevoProdu.price = req.body.price;
+
+    productos.push(nuevoProdu);
+
+    res.json(nuevoProdu);
+});
+
 
 router.get('/:id', (req, res) => {
     const {id} = req.params;
-    res.render(id)
-})
+    let produId =  productos.find(producto => producto.id == id);
+    
+    produId ? res.json(produId) : res.json({error: 'producto no encontrado '});
 
-router.post('/', (req, res) => {
-    res.render();
-});
+})
 
 router.put('/:id',(req,res)=>{
     const {id} = req.params;
-    res.render(id);
+    let produId =  productos.find(producto => producto.id == id);
+    
+    produId.name = req.body.name;
+    produId.description = req.body.description;
+    produId.price = req.body.price;
+
+    res.json(produId);
 });
 
 router.delete('/:id',(req,res)=>{
+    let newArray = [];
     const {id} = req.params;
-    res.render(id); 
+    newArray = productos.filter(producto => producto.id != id);
+    productos = [...newArray];
+
+    res.json(productos);
+
 });
 
 module.exports = router;
--- servidor.js
+++ servidor.js
@@ -10,23 +10,38 @@ const PORT = 8080;
 
 
 //middleware
-app.use(multer({
+
+/*app.use(multer({
     dest:__dirname + '/public/files',
 }).single('myFile'))
+*/
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))  
 
-//rutas
-app.use('/mascotas', require('./routes/routesMascotas.js'));
-app.use('/api/productos', require('./routes/routesApiProducts.js'));
 
-//ruta ejemplo con multer
-app.post('/uploadflie', (req, res) => {
+//Middleware de manejo de erro
+const paginaNoEncontrada = (err,req, res, next) => {
+    res.status(404).send('Pagina no encontrada');
+}
+
+//rutas
+//app.use('/mascotas', require('./routes/routesMascotas.js'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/producto.html')
+})
+app.use('/api/productos', require('./routes/routesApiProducts.js'));
+app.use(function(err, req, res, next){
+    res.status(500).send('Algo salio mal');
+})
+
+/*ruta ejemplo con multer
+    app.post('/uploadflie', (req, res) => {
     console.log(req.file)
     res.json({messenger: "subido exitosamente"})   
-})
+})*/
 
 
 //empezando servidor
